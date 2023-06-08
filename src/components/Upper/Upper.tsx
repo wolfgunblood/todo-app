f174ca53a91f63086ffd6f06933dea9a3419ddd7
wolfgunblood <jalajdorai1@gmail.com>
2023-06-08 21:19:09 +0530

    Updated Upper part

--- src/components/Upper/Upper.tsx
+++ src/components/Upper/Upper.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from 'react';
+import "./Upper.scss";
+import Light from "../../assets/images/icon-sun.svg"
 
 const Upper = () => {
   return (
-    <div>Upper</div>
+    <div className='upper-container'>
+      <div className='upper-header'>
+        <h3>TODO</h3>
+        <img src={Light} alt="Light Mode" className='icon-light'/>
+      </div>
+      <input type="text" className='todo-input' placeholder='What needs to be done?' />
+    </div>
   )
 }
 
